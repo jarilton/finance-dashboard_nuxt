98393059314e4e90e988eb3536ed066c1cf5d54b
jarilton <jariltonpereira02@hotmail.com>
2022-04-30 21:12:47 -0300

    busca de dados fetch

--- pages/servicos.vue
+++ pages/servicos.vue
@@ -8,12 +8,42 @@
 
     <br /><br />
 
+    <pre>
+      {{ $fetchState }}
+    </pre>
+
+    <div v-if="$fetchState.pending">Carregando...</div>
+
+    <div v-else>
+      <div
+        v-for="service in services"
+        :key="service.id"
+        class="border-b border-gray-400 py-3"
+      >
+        {{ service.username }}
+      </div>
+    </div>
+
     <NuxtChild />
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "",
+
+  data() {
+    return {
+      services: [],
+    };
+  },
+
+  async fetch() {
+    this.services = await this.$axios.$get(
+      "https://jsonplaceholder.typicode.com/users?_limit=3"
+    );
+  },
+};
 </script>
 
 <style>
