72b7c96d301c9c105d9a3c3d277d757376d710b6
jarilton <jariltonpereira02@hotmail.com>
2022-05-06 23:00:18 -0300

    loop dos dados das transações e format date

--- pages/index.vue
+++ pages/index.vue
@@ -132,15 +132,10 @@
         <div class="flex items-center justify-between">
           <h1 class="font-bold text-2xl">Transações</h1>
 
-          <AppButton @click="isAdding = !isAdding"> 
-            Nova transação 
-          </AppButton>
+          <AppButton @click="isAdding = !isAdding"> Nova transação </AppButton>
         </div>
 
-        <TransactionAdd 
-          v-if="isAdding"
-          @onCancel="isAdding = false"
-        />
+        <TransactionAdd v-if="isAdding" @onCancel="isAdding = false" />
 
         <div
           class="mt-6 pb-6 flex items-center space-x-4 border-b border-gray-300"
@@ -265,82 +260,18 @@
                     </div>
                   </div>
                 </div>
-
-                <div
-                  class="flex items-center px-5 py-6 bg-white rounded-lg shadow"
-                >
-                  <div class="flex items-center space-x-5">
-                    <div>
-                      <div>
-                        <div
-                          class="
-                            inline-flex
-                            items-center
-                            px-2.5
-                            py-0.5
-                            rounded-full
-                            text-sm
-                            font-medium
-                            bg-indigo-100
-                            text-indigo-800
-                          "
-                        >
-                          Software
-                        </div>
-                      </div>
-
-                      <div class="mt-1.5">Pagamento de boleto</div>
-                    </div>
-                  </div>
-
-                  <div class="flex items-center space-x-4 ml-auto">
-                    <div class="flex items-center">
-                      <svg
-                        class="w-4 h-4 text-red-600"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M20 12H4"
-                        ></path>
-                      </svg>
-
-                      <div class="font-bold">R$ 43,02</div>
-                    </div>
-
-                    <button>
-                      <svg
-                        class="w-4 h-4"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M19 9l-7 7-7-7"
-                        ></path>
-                      </svg>
-                    </button>
-                  </div>
-                </div>
               </div>
             </div>
 
-            <div>
+            <div v-for="(group, index) in transactionGrouped" :key="index">
               <div class="mb-1">
-                <div class="font-bold text-sm">04 de Jan</div>
+                <div class="font-bold text-sm">{{ formatDate(index) }}</div>
               </div>
 
               <div class="space-y-3">
                 <div
+                  v-for="transaction in group"
+                  :key="transaction.id"
                   class="flex items-center px-5 py-6 bg-white rounded-lg shadow"
                 >
                   <div class="flex items-center space-x-5">
@@ -359,11 +290,11 @@
                             text-indigo-800
                           "
                         >
-                          Software
+                          {{ transaction.category.name }}
                         </div>
                       </div>
 
-                      <div class="mt-1.5">Pagamento de boleto</div>
+                      <div class="mt-1.5">{{ transaction.description }}</div>
                     </div>
                   </div>
 
@@ -384,73 +315,7 @@
                         ></path>
                       </svg>
 
-                      <div class="font-bold">R$ 43,02</div>
-                    </div>
-
-                    <button>
-                      <svg
-                        class="w-4 h-4"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M19 9l-7 7-7-7"
-                        ></path>
-                      </svg>
-                    </button>
-                  </div>
-                </div>
-
-                <div
-                  class="flex items-center px-5 py-6 bg-white rounded-lg shadow"
-                >
-                  <div class="flex items-center space-x-5">
-                    <div>
-                      <div>
-                        <div
-                          class="
-                            inline-flex
-                            items-center
-                            px-2.5
-                            py-0.5
-                            rounded-full
-                            text-sm
-                            font-medium
-                            bg-indigo-100
-                            text-indigo-800
-                          "
-                        >
-                          Software
-                        </div>
-                      </div>
-
-                      <div class="mt-1.5">Pagamento de boleto</div>
-                    </div>
-                  </div>
-
-                  <div class="flex items-center space-x-4 ml-auto">
-                    <div class="flex items-center">
-                      <svg
-                        class="w-4 h-4 text-red-600"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M20 12H4"
-                        ></path>
-                      </svg>
-
-                      <div class="font-bold">R$ 43,02</div>
+                      <div class="font-bold">{{ transaction.amount }}</div>
                     </div>
 
                     <button>
@@ -481,6 +346,7 @@
 </template>
 
 <script>
+import { groupBy, orderBy } from "lodash";
 import AppButton from "~/components/UI/AppButton";
 import AppFormInput from "~/components/UI/AppFormInput";
 import AppFormLabel from "~/components/UI/AppFormLabel";
@@ -495,10 +361,27 @@ export default {
     AppFormSelect,
     TransactionAdd,
   },
+
+  async asyncData({ store }) {
+    return {
+      transactions: await store.dispatch("transactions/getTransactions"),
+    };
+  },
+
   data() {
     return {
       isAdding: false,
     };
+  },
+  computed: {
+    transactionGrouped() {
+      return groupBy(orderBy(this.transactions, "date", "desc"), "date");
+    },
+  },
+  methods: {
+    formatDate(date) {
+      return this.$dayjs(date).format("DD/MM/YYYY");
+    },
   },
 };
 </script>
